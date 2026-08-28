d9d01bd422b84218f073806fdd474bbe3f74e3d2
Rohan Tamulli <139643848+RTongit@users.noreply.github.com>
2026-08-28 18:37:01 +0530

    Update padding in AuthImagePattern component

--- frontend/src/app/components/AuthImagePattern.jsx
+++ frontend/src/app/components/AuthImagePattern.jsx
@@ -2,7 +2,7 @@
 
 export default function AuthImagePattern({title,subtitle}) {
   return (
-    <div className="hidden lg:flex items-center justify-center  p-12">
+    <div className="hidden lg:flex items-center justify-center  p-12 xl:p-13">
       <div className="max-w-md text-center">
         <div className="grid grid-cols-3 gap-3 mb-8">
           {[...Array(9)].map((_, i) => (
